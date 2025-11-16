f3f707d3cc8647f1e6fce64352cd1cf805c2dc0b
Hichem Benali <hichem.benali@email.com>
2025-11-16 13:57:44 +0100

    Assitances changes

--- cspa_assist/cspa_assist/doctype/formulaire_carglass/formulaire_carglass.js
+++ cspa_assist/cspa_assist/doctype/formulaire_carglass/formulaire_carglass.js
@@ -1,8 +1,14 @@
-// Copyright (c) 2025, SARL MYNDALL and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Formulaire CarGlass", {
+    refresh(frm) {
 
-// frappe.ui.form.on("Formulaire CarGlass", {
-// 	refresh(frm) {
+        // --- Ordre de service (PDF) ---
+        frm.add_custom_button(__('📄 Ordre de service (PDF)'), () => {
+            const format = "Ordre de service";
+            const url = frappe.urllib.get_full_url(
+                `/api/method/frappe.utils.print_format.download_pdf?doctype=${frm.doctype}&name=${frm.docname}&format=${encodeURIComponent(format)}&no_letterhead=0`
+            );
+            window.open(url);
+        });
 
-// 	},
-// });
+    }
+});
